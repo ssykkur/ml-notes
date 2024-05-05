1e435607e1267a7b2d24893ca3b7cdb7b46f29ee
ssykkur <ashembered@gmail.com>
2024-05-05 15:38:46 +0300

    newton's method example

---
+++ Nm.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+
+def f_1(x):
+    return np.exp(x) - np.log(x)
+
+def dfdx_1(x):
+    return np.exp(x) - 1/x
+
+def d2fdx2_1(x):
+    return np.exp(x) + 1/(x**2)
+
+x_0 = 1.6
+
+
+
+def newtons_method(dfdx, d2fdx2, x, num_iterations=100):
+    for i in range(num_iterations):
+        x = x - dfdx(x)/d2fdx2(x)
+        print(x)
+    return x
+
+num_iterations_example_1 = 25; x_initial = 1.6
+newtons_method(dfdx_1,d2fdx2_1, 1.6, 25)
+
+def f_2(x, y):
+    return x**4 + 0.8*y**4 + 4*x**2 + 2*y**2 - x*y -0.2*x**2*y
+
+def grad_f_2(x, y):
+    return np.array([[4*x**3 + 8*x - y - 0.4*x*y],
+                     [3.2*y**3 +4*y - x - 0.2*x**2]])
+
+def hessian_f_2(x, y):
+    hessian_f = np.array([[12*x**2 + 8 - 0.4*y, -1 - 0.4*x],
+                         [-1 - 0.4*x, 9.6*y**2 + 4]])
+    return hessian_f
+x_0, y_0 = 4, 4
+print(f"f{x_0, y_0} = {f_2(x_0, y_0)}")
+print(f"grad f{x_0, y_0} = \n{grad_f_2(x_0, y_0)}")
+print(f"H{x_0, y_0} = \n{hessian_f_2(x_0, y_0)}")
+
+def plot_f_cont_and_surf(f):
+    
+    fig = plt.figure( figsize=(10,5))
+    fig.canvas.toolbar_visible = False
+    fig.canvas.header_visible = False
+    fig.canvas.footer_visible = False
+    fig.set_facecolor('#ffffff')
+    gs = GridSpec(1, 2, figure=fig)
+    axc = fig.add_subplot(gs[0, 0])
+    axs = fig.add_subplot(gs[0, 1],  projection='3d')
+    
+    x_range = [-4, 5]
+    y_range = [-4, 5]
+    z_range = [0, 1200]
+    x = np.linspace(*x_range, 100)
+    y = np.linspace(*y_range, 100)
+    X,Y = np.meshgrid(x,y)
+    
+    cont = axc.contour(X, Y, f(X, Y), cmap='terrain', levels=18, linewidths=2, alpha=0.7)
+    axc.set_xlabel('$x$')
+    axc.set_ylabel('$y$')
+    axc.set_xlim(*x_range)
+    axc.set_ylim(*y_range)
+    axc.set_aspect("equal")
+    axc.autoscale(enable=False)
+    
+    surf = axs.plot_surface(X,Y, f(X,Y), cmap='terrain', 
+                    antialiased=True,cstride=1,rstride=1, alpha=0.69)
+    axs.set_xlabel('$x$')
+    axs.set_ylabel('$y$')
+    axs.set_zlabel('$f$')
+    axs.set_xlim(*x_range)
+    axs.set_ylim(*y_range)
+    axs.set_zlim(*z_range)
+    axs.view_init(elev=20, azim=-100)
+    axs.autoscale(enable=False)
+    plt.show()
+    return fig, axc, axs
+
+plot_f_cont_and_surf(f_2)
